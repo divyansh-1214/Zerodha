fa86336aa89ae9136b78177a343c0089e62cef65
divyansh-1214 <divyansh.sri23@gmail.com>
2024-12-22 10:58:16 +0530

    {commit_message}

---
+++ font-end/src/about/AboutPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Hero from './Hero';
+
+function PricingPage() {
+    return (
+      <>
+        <Hero />
+        <Team />
+      </>
+    );
+  }
+  
+  export default PricingPage;
